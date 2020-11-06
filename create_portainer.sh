2cf8e716a3266faf6ab851dfa57619c66f39799b
root <root@192.168.3.170>
2020-11-05 22:18:32 -0500

    upgrade to portainer 2.0

--- create_portainer.sh
+++ create_portainer.sh
@@ -1,9 +1,11 @@
-docker service create \
-    --name portainer \
-    --network portainer_agent_network \
-    --publish 9000:9000 \
-    --replicas=1 \
-    --constraint 'node.role == manager' \
-    --mount type=bind,source=/mnt/data/portainer_data,destination=/data \
-    --mount type=bind,src=//etc/localtime,dst=/etc/localtime \
-    portainer/portainer -H "tcp://tasks.portainer_agent:9001" --tlsskipverify
+docker stack deploy --compose-file=portainer-agent-stack.yml portainer
+#docker service create \
+#    --name portainer \
+#    --network portainer_agent_network \
+#    --publish 9000:9000 \
+#    --replicas=1 \
+#    --constraint 'node.role == manager' \
+#    --mount type=bind,source=/mnt/data/portainer_data,destination=/data \
+#    --mount type=bind,src=//etc/localtime,dst=/etc/localtime \
+#    portainer/portainer -H "tcp://tasks.portainer_agent:9001" --tlsskipverify
+docker stack deploy --compose-file=portainer-agent-stack.yml portainer
